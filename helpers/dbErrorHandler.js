5ef1183e97987e9abe83afa5066ddd7f1bf5dffc
Your Name <you@example.com>
2020-09-27 17:37:37 +0600

    friendly error message for duplicate email address

--- helpers/dbErrorHandler.js
+++ helpers/dbErrorHandler.js
@@ -4,9 +4,14 @@ exports.errorHandler = error =>{
     if (error.code ){
         switch (error.code){
             case 11000:
+                if (error.keyValue.email != null){
+                    message = `Duplicate email address ${error.keyValue.email} already exists!`;
+                }
                 break;
             default:
                 message = "Something went wrong";
         }
     }
+
+    return message;
 }
