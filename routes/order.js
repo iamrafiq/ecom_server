1c0b421f85cc9ab6f76a11dd01db231486064fa2
Your Name <you@example.com>
2020-10-09 02:01:27 +0600

    order status updating

--- routes/order.js
+++ routes/order.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById, addOrderToUserHistory } = require("../controllers/user");
-const { create, listOrders } = require("../controllers/order");
+const { create, listOrders, getStatusValues, orderById, updateOrderStatus } = require("../controllers/order");
 const { decreaseQuantity } = require("../controllers/product");
 
 router.post(
@@ -21,6 +21,23 @@ router.get(
     isAdmin,
     listOrders
   );
+  router.get(
+    "/order/status-values/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    getStatusValues
+  );
+
+  router.put(
+    "/order/:orderId/status/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    updateOrderStatus
+  );
 
 router.param("userId", userById);
+router.param("orderId", orderById);
+
 module.exports = router;
